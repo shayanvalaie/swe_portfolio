3ee3630b7945eabfd1c6519d722c736ff01868f5
Shayan Valaie <valaieshayan1@gmail.com>
2022-04-07 22:44:50 -0700

    project changes

--- portfolio/src/components/CardData.js
+++ portfolio/src/components/CardData.js
@@ -1,17 +1,10 @@
 import * as SiIcons from 'react-icons/si'
 import * as GoIcons from 'react-icons/go'
-import * as RiIcons from 'react-icons/ri'
+import * as GiIcons from 'react-icons/gi'
 
 
 export const CardData = [
-    {
-        image: <SiIcons.SiNetflix />,
-        title: 'Netflix Clone',
-        description: 'Clone of Netflix utilizing TMDB API',
-        live: 'https://netflix-clone-9d54e.web.app/',
-        gitHub: 'https://github.com/shayanvalaie/netflix-clone'
 
-    },
     {
         image: <GoIcons.GoCreditCard />,
         title: 'E-commerce UI',
@@ -21,11 +14,20 @@ export const CardData = [
 
     },
     {
-        image: <RiIcons.RiBoxingFill />,
-        title: 'Hustle Striking',
-        description: 'E-commerce application',
-        live: 'http://hustlestriking.com/',
-        gitHub: 'https://github.com/shayanvalaie/hustle_striking'
+        image: <SiIcons.SiNetflix />,
+        title: 'Netflix Clone',
+        description: 'Clone of Netflix utilizing TMDB API',
+        live: 'https://netflix-clone-9d54e.web.app/',
+        gitHub: 'https://github.com/shayanvalaie/netflix-clone'
+
+    },
+
+    {
+        image: <GiIcons.GiPencilBrush />,
+        title: 'Tattoo Template',
+        description: 'Tattoo Shop Template',
+        live: 'https://shayanvalaie.github.io/tattoo-template/',
+        gitHub: 'https://github.com/shayanvalaie/tattoo-template/'
 
     },
 
